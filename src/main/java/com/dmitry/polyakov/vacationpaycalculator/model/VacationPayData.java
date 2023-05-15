d69a98eb65cc8dfe9b2d95cde30ab6b8436ea680
AppleDim <pollydimmi@mail.ru>
2023-05-15 21:27:13 +0300

    added constructor with 2 parameters for tests

--- src/main/java/com/dmitry/polyakov/vacationpaycalculator/model/VacationPayData.java
+++ src/main/java/com/dmitry/polyakov/vacationpaycalculator/model/VacationPayData.java
@@ -1,5 +1,6 @@
 package com.dmitry.polyakov.vacationpaycalculator.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,8 +8,14 @@ import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class VacationPayData {
     private double averageAnnualSalary;
     private int vacationDays;
     private LocalDate startVacationDate; //month > 12 will be set to 12
+
+    public VacationPayData(double averageAnnualSalary, int vacationDays) {
+        this.averageAnnualSalary = averageAnnualSalary;
+        this.vacationDays = vacationDays;
+    }
 }
